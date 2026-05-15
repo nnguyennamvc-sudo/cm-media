e179ec60092447ab6866bd7d65523e2d615c5182
nnguyennamvc-sudo <nnguyennamvc@gmail.com>
2026-05-15 15:09:34 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="CM Media API")
  
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
